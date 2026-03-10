e8858cbf4205e1b8ac961df493a2e03ddc1dfbe2
PBSascha <pb-00-gebsas1@schulung-bbw.de>
2026-03-10 13:53:06 +0100

    feat: creates 6 creatures

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using Hagrids_CMS;
+
+List <Creature> creatures = new List<Creature>();
+creatures.Add(new Creature("Norbert", "Norwegian Ridgeback", "High", 2));
+creatures.Add(new Creature("Buckbeak", "Hippogriff", "Medium", 5));
+creatures.Add(new Creature("Aragog", "Acromantula", "High", 10));
+creatures.Add(new Creature("Fawkes", "Phoenix", "Low", 500));
+creatures.Add(new Creature("Fluffy", "Three-Headed Dog", "High", 3));
+creatures.Add(new Creature("Dobby", "House Elf", "Low", 100));
