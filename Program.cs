a44a40a437c2cf78e2c5f1db58b53ad0f4dbab9a
PBSascha <pb-00-gebsas1@schulung-bbw.de>
2026-03-10 13:54:29 +0100

    feat: creates 5 students

--- Program.cs
+++ Program.cs
@@ -7,3 +7,10 @@ creatures.Add(new Creature("Aragog", "Acromantula", "High", 10));
 creatures.Add(new Creature("Fawkes", "Phoenix", "Low", 500));
 creatures.Add(new Creature("Fluffy", "Three-Headed Dog", "High", 3));
 creatures.Add(new Creature("Dobby", "House Elf", "Low", 100));
+
+List <Student> students = new List<Student>();
+students.Add(new Student("Harry Potter", "Gryffindor", 11));
+students.Add(new Student("Hermione Granger", "Gryffindor", 11));
+students.Add(new Student("Ron Weasley", "Gryffindor", 11));
+students.Add(new Student("Draco Malfoy", "Slytherin", 11));
+students.Add(new Student("Luna Lovegood", "Ravenclaw", 11));
